e7d62862f21b4870fd3134c99458919af5c22864
Nick Werblun <nickwerblun@gmail.com>
2025-06-28 11:40:35 -0700

    dumb

--- goon_client.py
+++ goon_client.py
@@ -227,7 +227,7 @@ class GoonClient(discord.Client):
 
         @self.tree.command(
             name="magic_eight_balls",
-            description="CrabRangooner is currently experiencing post nut clarity and will surely provide exceptional assistance"
+            description="CrabRangooner is experiencing post-nut clarity and will provide exceptional assistance"
         )
         @discord.app_commands.describe(
             question="What would you like to ask?"
