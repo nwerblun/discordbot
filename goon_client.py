075fb21956294a4422f79c43cb0d6bed0ad60866
Nick Werblun <nickwerblun@gmail.com>
2025-06-28 11:38:35 -0700

    dumb

--- goon_client.py
+++ goon_client.py
@@ -232,9 +232,6 @@ class GoonClient(discord.Client):
         @discord.app_commands.describe(
             question="What would you like to ask?"
         )
-        @discord.app_commands.rename(
-            question="Your question"
-        )
         async def eight_ball(interaction: discord.Interaction, question: str):
             response = Responses.eight_ball_response()
             await interaction.response.send_message("<:thonk:1367947311627374592>" + response + "<:thonk:1367947311627374592>")
